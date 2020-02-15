e950500bfc146baf3d6977ec8fe027477d440850
joanTrave <jtravego7@alumnes.ub.edu>
2020-02-15 22:27:35 +0100

    Més test definits

--- src/test/java/ComUtilsTest.java
+++ src/test/java/ComUtilsTest.java
@@ -10,15 +10,35 @@ import java.io.IOException;
 public class ComUtilsTest {
 
     @Test
-    public void example_test() {
-        File file = new File("test");
+    public void int_test() {
+        File file = new File("test.txt");
+        int _example_int = 1;
+
         try {
             file.createNewFile();
             ComUtils comUtils = new ComUtils(new FileInputStream(file), new FileOutputStream(file));
-            comUtils.write_int32(2);
-            int readedInt = comUtils.read_int32();
+            comUtils.write_int32(_example_int);
+            int _readed_int = comUtils.read_int32();
 
-            assertEquals(2, readedInt);
+            assertEquals(_example_int, _readed_int);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void char_test() {
+        File file = new File("test.txt");
+        char _example_char = 'a';
+
+        try {
+            file.createNewFile();
+            ComUtils comUtils = new ComUtils(new FileInputStream(file), new FileOutputStream(file));
+            comUtils.write_char(_example_char);
+            char _readed_char = comUtils.read_char();
+
+            assertEquals(_example_char, _readed_char);
 
         } catch (IOException e) {
             e.printStackTrace();
